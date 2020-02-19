7c9ca61e588cde076dbbc18d6fa21dce8d9bb64b
Kostya <goncharovk911@gmail.com>
2020-02-19 16:37:02 +0200

    React Project

---
+++ react_my_project/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Navbar = () => {
+    return(
+        <nav className= 'nav'>
+        <ul >
+          <li>
+             <a>Profile</a>
+           </li>
+           <li>
+             <a>Message</a>
+           </li>
+           <li>
+             <a>News</a>
+           </li>
+           <li>
+             <a>Music</a>
+           </li>
+           <li>
+             <a>Settings</a>
+           </li>                       
+         </ul>
+      </nav>
+    );
+}
+
+export default Navbar;
---
+++ react_my_project/src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () =>{
+    return(
+    <div className= 'content'>
+        <div>
+          <img src='https://unsplash.com/photos/LY1eyQMFeyo/download' />
+        </div>
+        <div>
+          ava+description
+        </div>
+        <div>
+          My posts
+          <div>
+            New posts
+          </div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile;
